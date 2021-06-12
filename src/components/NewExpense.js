5265d544d2f01a9be7ebdf58ba1430e2269380c6
Bishnu Kumar Das <kumarbishnu202@gmail.com>
2021-06-12 22:38:44 +0545

    Added functionality to NewExpense component

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,32 +1,62 @@
 import {Button, Card, Col, Form, InputGroup, Row} from "react-bootstrap";
+import {useState} from "react";
 
 const NewExpense = () => {
 
-    return <Card body bg={"dark"} className="mb-3 w-75 mx-auto">
-        <Row as={"form"} >
-            <Col md={6} className="mb-3">
-                <InputGroup>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Title</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        type="text"
-                    />
-                </InputGroup>
-            </Col>
-            <Col md={6} className="mb-3">
-                <InputGroup>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>Amount</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        type="number"
-                        min="0.01"
-                        step="0.01"
-                    />
-                </InputGroup>
-            </Col>
-            <Col md={6} className="mb-3">
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const titleChange = event => {setTitle(event.target.value);}
+    const amountChange = event => {setAmount(event.target.value);}
+    const dateChange = event => {setDate(event.target.value);}
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const expense = {
+            title: title,
+            amount: amount,
+            date: date,
+        }
+        console.log(expense);
+        clearForm()
+    }
+
+    const clearForm = () => {
+        setTitle('');
+        setAmount('');
+        setDate('');
+    }
+
+    return <Card as={"form"} onSubmit={submitHandler} body bg={"dark"} className="mb-3 w-75 mx-auto">
+            <Row>
+                <Col md={6} className="mb-3">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>Title</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                            type="text"
+                            value={title}
+                            onChange={titleChange}
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} className="mb-3">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>Amount</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                            type="number"
+                            min="0.01"
+                            step="0.01"
+                            value={amount}
+                            onChange={amountChange}
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} className="mb-3">
                 <InputGroup>
                     <InputGroup.Prepend>
                         <InputGroup.Text>Date</InputGroup.Text>
@@ -35,11 +65,13 @@ const NewExpense = () => {
                         type="date"
                         min="2019-01-01"
                         max="2022-12-31"
+                        value={date}
+                        onChange={dateChange}
                     />
                 </InputGroup>
             </Col>
-        </Row>
-        <Button variant="success" className="float-end px-5" type="submit">Add</Button>
+            </Row>
+            <Button variant="success" className="float-end px-5" type="submit">Add</Button>
     </Card>
 }
 
